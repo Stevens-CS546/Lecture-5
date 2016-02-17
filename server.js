07b043b3a2339527d6229896db8d746fb48810f4
Phil Barresi <me@philbarresi.com>
2016-02-17 15:43:21 -0500

    Checkpoint

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // We first require our express package
 var express = require('express');
 var bodyParser = require('body-parser');
-var myData = require('./data.js')
+var questionAndAnswers = require('./data.js')
 
 // This package exports the function to create an express instance:
 var app = express();
+
+// Here we change our view engine from Jade (default) to EJS
+app.set('view engine', 'ejs');  
 
 // This is called 'adding middleware', or things that will help parse your request
 app.use(bodyParser.json()); // for parsing application/json
@@ -17,12 +20,45 @@ app.use('/assets', express.static('static'));
 
 // Setup your routes here!
 
-app.get("/", function (request, response) { 
-    // We have to pass a second parameter to specify the root directory
-    // __dirname is a global variable representing the file directory you are currently in
-    response.sendFile("./pages/index.html", { root: __dirname });
+app.get("/", function (request, response) {
+    response.redirect("/questions");
 });
 
+app.get("/questions/:id", function (request, response) {
+    try {
+        var question = questionAndAnswers.getQuestion(request.params.id);
+        // we caught an exception! Let's show an error page!
+        response.status(500).render('pages/question', { question: question });
+    } catch (message) {
+        // we caught an exception! Let's show an error page!
+        response.status(500).render('pages/error', { errorType: "Issue loading question!", errorMessage: message });
+    }
+});
+
+app.get("/questions", function (request, response) {
+    // We have to pass a second parameter to specify the root directory
+    // __dirname is a global variable representing the file directory you are currently in
+    var questionType = request.query.type,
+        displayType = "",
+        questionsToShow = [];
+
+    // First, we retrieve our data
+    if (questionType === "answered") {
+        displayType = "Answered Questions";
+        questionsToShow = questionAndAnswers.getAnsweredQuestions();
+    } else if (questionType === "all") {
+        displayType = "All Questions";
+        questionsToShow = questionAndAnswers.getAllQuestions();
+    } else {
+        displayType = "Unanswered Questions";
+        questionsToShow = questionAndAnswers.getUnansweredQuestions();
+    }
+
+    // render will search your 'views' directory and follow the path you give it to get a template
+    // it will compile the template with the model you provide in the second parameter and
+    // send it to the user
+    response.render('pages/index', { questions: questionsToShow, type: displayType });
+});
 
 // We can now navigate to localhost:3000
 app.listen(3000, function () {
